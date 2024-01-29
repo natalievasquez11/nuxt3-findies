e3d85ea39acfdc488835c94d6d4354e7414f73af
Natalie Vasquez <natalievasquez11@gmail.com>
2024-01-28 19:27:24 -0600

    ssr configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   devtools: { enabled: true },
   typescript: {
     shim: false
@@ -14,8 +15,5 @@ export default defineNuxtConfig({
   },
   nitro: {
     preset: 'node-server',
-    prerender: {
-      routes: ['/nuxt3-fundies']
-    }
   },
 })
